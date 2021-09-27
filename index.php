625ee653218f361f8d0647a216947b53f2c6038f
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2021-09-27 19:43:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,76 @@
-alex alllasdlasdfkjh 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>TÍTULO DO SITE</title>
+</head>
+ <style>
+    body{
+      font-family: 'Titillium Web', sans-serif;
+      color: #666666;
+      text-align: left;
+      background-image: url(bkp/images.jpg);
+    }
+    .box{
+      background-color: #9fc5c7;
+      width: 800px;
+      margin: 10px auto;
+    }
+
+    .box1{
+      background-color: #c7b39f;
+      width: 800px;
+      margin: 10px auto;
+    }
+
+    #title {
+      font-size: 30px;
+      margin-top: 25px;
+    }
+    #descricao{
+      font-size: 18px;
+      margin: 15px auto;
+    }
+    #logo {
+      margin-top: 40px;
+      max-width: 365px;
+      height: auto;
+    }
+    #logo-task {
+      width: 75px;
+
+    }
+  </style>
+<body>
+  <div class="box">
+
+    <div id="title">
+<center>MENU:</center>
+</div>
+<div>
+<center>
+<a href="criar-db.php"><button>Criar Banco</button></a>
+<a href="criar-tabelas-fixa.php"><button>Criar Tabela</button></a>
+<a href="insert-postgres.php"><button>Inserir Dados</button></a>
+<a href="consultar.php"><button>Consultar Full</button></a>
+<a href="consultar-registro.php"><button>Consultar Codigo</button></a>
+<a href="deletar.php"><button>Deletar</button></a>
+</center>
+</div>
+
+</body>
+
+<body>
+  <div class="box1">
+
+    <div id="title">
+<center>TESTE:</center>
+</div>
+<div>
+<a href="https://wipsites.com.br"><button>Meu Botão</button></a>
+
+</div>
+
+</body>
+
+
+</html
